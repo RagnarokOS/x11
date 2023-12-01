a31d627c4ce2698ff676095bd2042070d0a935d7
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-12-01 14:33:18 -0500

    remove -g
    Wasn't there by default, and it causes building the package to fail.

--- progs/wmutils-core/config.mk
+++ progs/wmutils-core/config.mk
@@ -7,6 +7,6 @@ LD      = $(CC)
 HARDENING_CFLAGS = -D_FORTIFY_SOURCE=2 -Wformat-security -fstack-protector-strong -fstack-clash-protection --param=ssp-buffer-size=4 -fcf-protection
 HARDENING_LDFLAGS = -Wl,-z,relro,-z,now -Wl,--as-needed
 
-CFLAGS  = -std=c99 -pedantic -Wall -g -O2 ${CFLAGS_LTO} ${HARDENING_CFLAGS} -I/usr/X11R6/include
+CFLAGS  = -std=c99 -pedantic -Wall -O2 ${CFLAGS_LTO} ${HARDENING_CFLAGS} -I/usr/X11R6/include
 LDFLAGS = -lxcb -lxcb-util -lxcb-cursor ${LDFLAGS_LTO} ${HARDENING_LDFLAGS}  -L/usr/X11R6/lib
 
